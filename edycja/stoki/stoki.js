4c545c154cdcb142f6051e09e85fed041f6c75a7
unknown <bartekmachu@gmail.com>
2021-10-23 16:16:26 +0200

    add new page stoki magazynowe, finish styling

---
+++ edycja/stoki/stoki.js
@@ -0,0 +1,123 @@
+let content = {
+
+
+
+    /* 
+        TUTAJ EDYTUJESZ NAPISY DO STRONY O FIRMIE
+    */
+
+        glownyNapis1: "stoki magazynowe",
+        naglowek1: "Kluczowe informacje",
+        paragraf1: "Tutaj troche tekstu reklamowego opwiadającego o firmie Tutaj troche tekstu reklamowego opwiadającego o firmie Tutaj troche tekstu reklamowego opwiadającego o firmie ",
+
+
+    /* 
+        TUTAJ EDYTUJESZ NAPISY DO BANERÓW
+    */
+
+    //baner numer 1
+
+        baner1naglowek: "ELASTYCZNOŚĆ",
+        baner1paragraf: "Przyjmujemy bardzo szeroki wachlarz towarów. Cechuje nas elastyczność",
+
+    //baner numer 2
+
+        baner2naglowek: "EFEKTYWNOŚĆ",
+        baner2paragraf: "Działamy na podstawie dostarczonej specyfikacji towaru. Szybko i efektywnie.",
+
+    //baner numer3
+
+        baner3naglowek: "REKLAMOWY TEKST",
+        baner3paragraf: "Przyjmujemy bardzo szeroki wachlarz towarów. Cechuje nas elastyczność.",
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const h1 = document.querySelector('.dashboard-mini-container__heading-holder--heading');
+const h2 = document.querySelector('.dashboard-mini-container__subheading-holder--subheading');
+const p = document.querySelector('.dashboard-mini-container__paragraph-holder--paragraph');
+
+const banerHeading1 = document.querySelector('.stocks-baner-heading-1');
+const banerParagraph1 = document.querySelector('.stocks-baner-paragraph-1');
+
+const banerHeading2 = document.querySelector('.stocks-baner-heading-2');
+const banerParagraph2 = document.querySelector('.stocks-baner-paragraph-2');
+
+const banerHeading3 = document.querySelector('.stocks-baner-heading-3');
+const banerParagraph3 = document.querySelector('.stocks-baner-paragraph-3');
+
+h1.innerHTML = content.glownyNapis1;
+h2.innerHTML = content.naglowek1;
+p.innerHTML = content.paragraf1;
+
+banerHeading1.innerHTML = content.baner1naglowek;
+banerParagraph1.innerHTML = content.baner1paragraf;
+
+banerHeading2.innerHTML = content.baner2naglowek;
+banerParagraph2.innerHTML = content.baner2paragraf;
+
+banerHeading3.innerHTML = content.baner3naglowek;
+banerParagraph3.innerHTML = content.baner3paragraf;
--- scripts/components/Dashboard-primary/dashboard-primary-slider.js
+++ scripts/components/Dashboard-primary/dashboard-primary-slider.js
@@ -1,4 +1,8 @@
 var swiper = new Swiper(".mySwiper", {
+  autoplay: {
+    delay: 6000,
+    disableOnInteraction: false,
+  },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
